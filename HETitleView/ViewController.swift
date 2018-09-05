7365a3225234173693f1376edff6c16fc6156dd3
heyode <1025335931@qq.com>
2018-09-05 11:01:58 +0800

    add note

--- HETitleView/ViewController.swift
+++ HETitleView/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+        let titleLab = UILabel.init(frame: CGRect.init(x: (UIScreen.main.bounds.width - 300)/2, y: 200, width: 300, height: 30))
+        titleLab.textAlignment = .center
+        titleLab.text = "title"
+        titleLab.textColor = UIColor.init(red: 22.0/255.0, green: 157.0/255.0, blue: 240.0/255.0, alpha: 1)
+        view.addSubview(titleLab)
+        
         let config = HETitleViewConfig()
         let titles = ["全部","外科","内科","生殖科","耳鼻喉科","内科","眼科","口腔科"]
-        let titleView = HETitleView.init(frame: CGRect.init(x: 0, y: 80, width: UIScreen.main.bounds.size.width, height: 59), titles:titles ,titleViewConfig:config)
+        
+        let titleView = HETitleView.init(frame: CGRect.init(x: 0, y: 80, width: UIScreen.main.bounds.size.width, height: 59),
+                                         titles:titles,
+                                         titleViewConfig:config)
+        
         view.addSubview(titleView)
+        
+        
         titleView.titleViewClickCallBack = { selectIndex in
-            NSLog("点击了:%@", titles[selectIndex])
+           titleLab.text = titles[selectIndex]
         }
     }
 
--- HETitleView/source/HEPageView.swift
+++ HETitleView/source/HEPageView.swift
@@ -8,27 +8,41 @@
 
 import UIKit
 class HETitleViewConfig : NSObject{
+    // 是否显示指示器
     open var showIndicator : Bool = true
+    // 是否显示底部分割线
     open var showBottomLineView : Bool = true
+    // 是否显示右侧有隐藏内容的标识
     open var showRightImageView : Bool = true
+    // 指示器的宽度类型
     open var indicatorViewWidthType = HETitleViewIndicatorViewWidth.adaptivelyWidth
-    /// 根据文字内容宽度再加上额外的宽度
+    // 指示器到底部分割线的距离
     open var indicatorViewBottomToBottomLineMargin : CGFloat = 8
+    // 在文字内容宽度再加上额外的宽度（只在indicatorViewWidthType=.adaptivelyWidth生效）
     open var indicatorViewAdditionalWith : CGFloat  = 0
-    open var indicatorViewHeight : CGFloat = 2
+    // 指示器的固定宽度（只在indicatorViewWidthType=.fixedWidthin生效）
     open var indicatorViewWidth : CGFloat = 20
+    // 指示器的高度
+    open var indicatorViewHeight : CGFloat = 2
+    // 指示器背景色
     open var indicatorViewBackgroudColor  : UIColor = UIColor.init(red: 22.0/255.0, green: 157.0/255.0, blue: 240.0/255.0, alpha: 1)
+    // 每次点击指示器的滑动时间
     open var indicatorViewAnimation : Double = 0.25
+    // 指示器圆角度数
     open var indicatorViewRadius : CGFloat = 0
-    
+    // 底部分割线的高度
     open var bottomLineViewHeight : CGFloat = 1
+    // 底部分割线的背景色
     open var bottomLineViewBackgroudColor : UIColor = UIColor.init(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 1)
-    
+    // title的字体
     open var titleFont = UIFont.systemFont(ofSize: 15)
+    // title的颜色
     open var titleColor = UIColor.init(red: 30.0/255.0, green: 46/255.0, blue: 68/255.0, alpha:1)
+    // title被选中时的颜色
     open var selectedTitleColor = UIColor.init(red: 22.0/255.0, green: 157.0/255.0, blue: 240.0/255.0, alpha: 1)
+    // title除去文字，剩余的margin宽度
     open var titleAdditionalWith : CGFloat = 30
-    
+    // scrollView水平方向的左右内边距
     open var scrollViewHorizontalMargin : CGFloat = 20
     
 }
@@ -39,6 +53,7 @@ enum HETitleViewIndicatorViewWidth {
 
 class HETitleView: UIView {
     typealias titleViewBlock = (_ selectedIndex:Int)->Void
+    // 点击title的回调
     open var titleViewClickCallBack : titleViewBlock?
     private var config = HETitleViewConfig()
     private let btnBaseTag = 400
